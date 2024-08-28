bc7c826fa6528ce7ee52731b130b696103c0d456
Daniil Sivozhelezov <sivozelezovdaniil@gmail.com>
2024-08-28 16:28:19 +0500

    Create Custom Cell and add method for toggle completed

---
+++ ToDoList/CustonUI/CustomLabel.swift
@@ -0,0 +1,31 @@
+//
+//  CustomLabel.swift
+//  ToDoList
+//
+//  Created by Даниил Сивожелезов on 28.08.2024.
+//
+
+import UIKit
+
+final class CustomLabel: UILabel {
+    var size: UIFont
+    var color: UIColor
+    
+    init(size: UIFont, color: UIColor) {
+        self.size = size
+        self.color = color
+        
+        super.init(frame: .zero)
+        createLabel()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func createLabel() {
+        self.font = size
+        self.textColor = color
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
